1bde5f10b388242a40b9af76c552e8893e071b9b
paolobertellini <225184@studenti.unimore.it>
2019-09-29 09:55:13 +0200

    Added test metrics

--- simple_conv_net.py
+++ simple_conv_net.py
@@ -1,7 +1,11 @@
 import argparse
 from pathlib import Path
 
+
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -15,6 +19,7 @@ from model import SimpleCNN, CNN
 
 
 plt.interactive(True)
+plt.show(block=True)
 
 
 def main(args):
@@ -28,14 +33,15 @@ def main(args):
         transforms.Normalize((0.6242, 0.6232, 0.5952), (0.8963, 0.8787, 0.8833))])
 
     # -- DATASET -- #
-    car_trainset = CarDataset(dataset_dir=args.dataset_dir / 'train',
+    trainset = CarDataset(dataset_dir=args.dataset_dir / 'train',
                               transform=transform)
-    datasetHistogram(car_trainset.labels)
+    #datasetHistogram(trainset.labels)
+    trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
 
-    car_trainloader = DataLoader(car_trainset, batch_size=args.batch_size, shuffle=True)
-    car_testset = CarDataset(dataset_dir=args.dataset_dir / 'test',
+    testset = CarDataset(dataset_dir=args.dataset_dir / 'test',
                              transform=transform)
-    car_testloader = DataLoader(car_testset, batch_size=args.batch_size, shuffle=True)
+    testloader = DataLoader(testset, batch_size=args.batch_size, shuffle=True)
+
 
     # -- CONV NET -- #
     net = CNN().to(args.device)
@@ -44,26 +50,29 @@ def main(args):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+    loss_avg = 0
+    count = 0
+
     # -- TRAINING -- #
+
+    losses = []
+
     for epoch in range(args.epochs):
 
         avg_loss = []
-        losses = []
 
-        for batch in tqdm(car_trainloader, total=len(car_trainloader)):
+        for batch in tqdm(trainloader, total=len(trainloader)):
 
             inputs = batch['image']
             inputs = inputs.float().to(args.device)
+            labels = batch['idx'].to(args.device)
 
             outputs = net(inputs)
-
             optimizer.zero_grad()
 
-            labels = batch['idx'].to(args.device)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
             avg_loss.append(loss.item())
 
         avg_loss = np.asarray(avg_loss).mean()
@@ -72,7 +81,7 @@ def main(args):
         plt.show()
         print(f'Average loss for epoch {epoch}: {avg_loss:.3f}')
 
-    print('Finished Training')
+    print('Finished Training, losses:', losses)
 
 
 
@@ -81,31 +90,43 @@ def main(args):
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch in tqdm(car_testloader, total=len(car_testloader)):
+        for i, batch in tqdm(enumerate(testloader), total=len(testloader)):
             images, labels = batch['image'], batch['idx']
+            images = images.float().to(args.device)
             outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
+
+            # print("OUT DATA: ", torch.max(outputs.data, 1))
+            # print("PREDICTED: ", _, predicted)
+            # print("PREDICTED 2: ", predicted.data.cpu() )
+            # print("LABELS: ", labels, labels.size(0))
+
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            for i in range(labels.size(0)):
+                if (predicted.data.cpu()[i] == labels[i]):
+                    correct += 1
+
+            print(f"BATCH TEST {i}: predicted {predicted.data.cpu()} true {labels}")
 
     print(f"Accuracy of the network on the test images: {100 * correct / total}%")
-
-    class_correct = list(0. for i in range(10))
-    class_total = list(0. for i in range(10))
-    with torch.no_grad():
-        for batch in tqdm(car_testloader, total=len(car_testloader)):
-            images, labels = batch['image'], batch['idx']
-            outputs = net(images)
-            _, predicted = torch.max(outputs, 1)
-            c = (predicted == labels).squeeze()
-            for i in range(args.batch_size):
-                label = labels[i]
-                class_correct[label] += c[i].item()
-                class_total[label] += 1
-
-    for i in range(10):
-        print('Accuracy of %5s : %2d %%' % (
-            classes[i], 100 * class_correct[i] / class_total[i]))
+    #
+    # class_correct = list(0. for i in range(10))
+    # class_total = list(0. for i in range(10))
+    # with torch.no_grad():
+    #     for batch in tqdm(testloader, total=len(testloader)):
+    #         images, labels = batch['image'], batch['idx']
+    #         outputs = net(images)
+    #         images = images.float().to(args.device)
+    #         _, predicted = torch.max(outputs, 1)
+    #         c = (predicted == labels).squeeze()
+    #         for i in range(args.batch_size):
+    #             label = labels[i]
+    #             class_correct[label] += c[i].item()
+    #             class_total[label] += 1
+    #
+    # for i in range(10):
+    #     print('Accuracy of %5s : %2d %%' % (
+    #         classes[i], 100 * class_correct[i] / class_total[i]))
 
 
 if __name__ == '__main__':
@@ -114,10 +135,10 @@ if __name__ == '__main__':
     parser.add_argument('dataset_dir', type=Path)
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--batch_size', type=int, default=4)
-    parser.add_argument('--device', choices=['cpu', 'cuda'], default='cpu')
+    parser.add_argument('--device', choices=['cpu', 'cuda'], default='cuda')
     args = parser.parse_args()
-    #main(args)
-    from PIL import Image
-    plt.imshow(Image.open(args.dataset_dir / 'test' / 'img' / '000000.png'))
-    plt.show()
-    print(Image.open(args.dataset_dir / 'test' / 'img' / '000000.png').shape)
+    main(args)
+    # from PIL import Image
+    # plt.imshow(Image.open(args.dataset_dir / 'test' / 'img' / '000000.png'))
+    # plt.show(block=True)
+    # print(Image.open(args.dataset_dir / 'test' / 'img' / '000000.png').size)
